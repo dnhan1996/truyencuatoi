00e63b7fdea94bb111580a1254c12f4819402fdd
ngo tran duc nhan <dnhan1996@gmail.com>
2022-04-28 11:51:37 +0700

    insert index

--- src/Page.jsx
+++ src/Page.jsx
@@ -10,6 +10,7 @@ export default function Pages(){
     return <>
         <Routes>
             <Route path="/index" element={<Index />} />
+            <Route path="/" element={<Index />} />
             <Route path="/sign-in" element={<SignIn />} />
             <Route path="/sign-up" element={<SignUp />} />
             <Route path="/truyen/:novelId" element={<Details/>} />
